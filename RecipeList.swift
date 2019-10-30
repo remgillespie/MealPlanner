2381f03ae92a84d8cf67c0254119704dd91060b5
Matthew Middleton <mattmidd6@gmail.com>
2019-10-29 21:43:17 -0400

    Basic structure for the RecipeList

---
+++ RecipeList.swift
@@ -0,0 +1,24 @@
+public class RecipeList
+{
+	private list: List<Recipe>()
+	
+	public display_recipe(with_name name:Strings)
+	{
+		
+	}
+	
+	public display_recipe_names()
+	{
+		
+	}
+	
+	public add_recipe(using item: Recipe)
+	{
+		
+	}
+	
+	public remove_recipe(using name: String) -> Ingredients
+	{
+		
+	}
+}
