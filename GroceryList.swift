fcc75f09146a1c52256d34d629858c232fe8740a
Matthew Middleton <mattmidd6@gmail.com>
2019-11-04 22:15:56 -0500

    Removed confusing/unclear statement

--- GroceryList.swift
+++ GroceryList.swift
@@ -1,20 +1,21 @@
-/*Note: This class is 
+/*Note: This class is incomplete*/
+
 public class GroceryList
 {
-	private var list = Set<Ingredient>()
-	var size = 0
+	private var list = Set<(Ingredient, Bool)>()
+	public var size = 0
+	private var notes = ""
 	
 	/*Adds an item to the grocery list and increments the size of the list
 	if the item did not already exist in the list.
 	Returns true upon successful insertion, false otherwise.
 	*/
-	func add_item(to_list item: Ingredient)-> Bool
+	public func add_item(to_list item: Ingredient) -> Bool
 	{
-		var valid_insert = list.insert(Ingredient)
+		var valid_insert = list.insert((Ingredient,0))
 		if(valid_insert.0==true)
 		{
 			self.size += 1
-			
 		}
 		return valid_insert.0
 	}
@@ -22,8 +23,32 @@ public class GroceryList
 	/*Removes an item from the list.
 	Returns nil if the item was not in the list, otherwise the item is returned
 	*/
-	func remove_item(from_list item: Ingredient) -> Ingredient?
+	
+	//not done
+	public func remove_item(from_list item: Ingredient) -> Ingredient?
 	{
-		return list.remove(item)
+		return list.remove()
+	}
+	
+	/*Checks an item off a grocery list
+	*/
+	
+	//not done
+	public check_item(from_list item:Ingredient) -> Bool
+	{
+		return 0
+	}
+	
+	public display_list()
+	{
+		var current = 0
+		
+	}
+	
+	/*Adds/modifies notes
+		should allow for interactive input*/
+	public modify_notes()
+	{
+		
 	}
 }
